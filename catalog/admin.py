9540e4e839084fddd37712da48e0b2ad33f97b4c
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-12-27 23:10:05 +0300

    task_4 init

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from catalog.models import Product, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_category')
+    search_fields = ('name_category', 'description_category')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','name_product', 'price_product', 'category_product')
+    list_filter = ('category_product',)
+    search_fields = ('name_product', 'description_product')
+
--- catalog/models.py
+++ catalog/models.py
@@ -1,32 +1,33 @@
 from django.db import models
 
 
-class Product(models.Model):
-    name_product = models.CharField(max_length=150, verbose_name='Наименование продукта')
-    description_product = models.TextField(verbose_name='Описание продукта')
-    image_product = models.ImageField(upload_to='images/', verbose_name='Изображения')
-    category_product = models.CharField(max_length=150, verbose_name='Категория продукта')
-    price_product = models.FloatField
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.name_product}'
-
-    class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['name_product']
-
-
 class Category(models.Model):
     name_category = models.CharField(max_length=150, verbose_name='Наименование категории')
     description_category = models.TextField(verbose_name='Описание категории')
 
     def __str__(self):
-        return f'{self.name_category}'
+        return self.name_category
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         ordering = ['name_category']
+
+class Product(models.Model):
+    name_product = models.CharField(max_length=150, verbose_name='Наименование продукта')
+    description_product = models.TextField(verbose_name='Описание продукта')
+    image_product = models.ImageField(upload_to='images/', verbose_name='Изображения')
+    category_product = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='Категория продукта', null=True, blank=True, related_name='Products')
+    price_product = models.FloatField(verbose_name='Цена продукта', default=0.0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name_product
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name_product', 'category_product']
+
+
