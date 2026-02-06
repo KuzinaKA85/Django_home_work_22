5074a34b267a8423136f6750855ce107346ab08b
Kseniya Kuzina <kolosova_ka@mail.ru>
2026-02-06 23:21:58 +0300

    hw_27 добавлено ограничение доступа к страницам продуктов

--- config/settings.py
+++ config/settings.py
@@ -127,4 +127,6 @@ AUTH_USER_MODEL = "users.User"
 
 LOGIN_REDIRECT_URL = "/"
 
+LOGIN_URL = "/"
+
 # LOGOUT_REDIRECT_URL = "/"
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
 
@@ -9,7 +10,7 @@ from users.forms import UserRegisterForm
 from users.models import User
 
 
-class UserRegisterView(CreateView):
+class UserRegisterView(LoginRequiredMixin, CreateView):
     model = User
     form_class = UserRegisterForm
     template_name = "users/user_form.html"
@@ -24,12 +25,4 @@ class UserRegisterView(CreateView):
             from_email=EMAIL_HOST_USER,
             recipient_list=[user.email],
         )
-        # send_mail(subject, message, from_email, recipient_list)
         return super().form_valid(form)
-
-    # def send_welcome_email(self, user_email):
-    #     subject = 'Добро пожаловать в наш сервис'
-    #     message = 'Спасибо, что зарегистрировались в нашем сервисе!'
-    #     from_email = EMAIL_HOST_USER
-    #     recipient_list = [user_email]
-    #     send_mail(subject, message, from_email, recipient_list)
