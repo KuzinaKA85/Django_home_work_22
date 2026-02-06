e1900a335ed52dd0f5d0c48dda92f0615a3343d4
Kseniya Kuzina <kolosova_ka@mail.ru>
2026-02-06 23:12:09 +0300

    hw_27 отправка приветствия на email

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import login
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 
 from django.views.generic import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from users.forms import UserRegisterForm
 from users.models import User
 
@@ -11,3 +14,22 @@ class UserRegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = "users/user_form.html"
     success_url = reverse_lazy("users:login")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        send_mail(
+            subject="Добро пожаловать в наш сервис",
+            message="Спасибо, что зарегистрировались в нашем сервисе!",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        # send_mail(subject, message, from_email, recipient_list)
+        return super().form_valid(form)
+
+    # def send_welcome_email(self, user_email):
+    #     subject = 'Добро пожаловать в наш сервис'
+    #     message = 'Спасибо, что зарегистрировались в нашем сервисе!'
+    #     from_email = EMAIL_HOST_USER
+    #     recipient_list = [user_email]
+    #     send_mail(subject, message, from_email, recipient_list)
