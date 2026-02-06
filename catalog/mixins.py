ffbfb917bc6b87a6ce5f7bdd7030e8ad79e244c5
Kseniya Kuzina <kolosova_ka@mail.ru>
2026-02-06 21:29:59 +0300

    hw_27 авторизация, регистрация

--- catalog/mixins.py
+++ catalog/mixins.py
@@ -2,4 +2,4 @@ class ProductFormMixin:
     def __init__(self, *args, **kwargs):
         super(ProductFormMixin, self).__init__(*args, **kwargs)
         for field in self.fields.values():
-            field.widget.attrs.update({"class": "form-control"})
+            field.widget.attrs.update({"class": "form-control mb-3"})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic import CreateView
+
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = "users/user_form.html"
+    success_url = reverse_lazy("users:login")
