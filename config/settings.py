78080e8687e4532c83635d3dd9141858c1144ada
Kseniya Kuzina <kolosova_ka@mail.ru>
2026-02-10 21:39:10 +0300

    hw_29 Установлен Redis, добавлены настройки в settings.py

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,10 @@ LOGIN_REDIRECT_URL = "/"
 LOGIN_URL = "/"
 
 # LOGOUT_REDIRECT_URL = "/"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+    }
+}
